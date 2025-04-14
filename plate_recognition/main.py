fd99cb91ad88203c5e8efb1f74680983b8cafb04
Zarif <zarif.hossain1909@gmail.com>
2025-04-14 23:37:39 +1000

    add TODOs to help track progress of project

---
+++ plate_recognition/main.py
@@ -0,0 +1,31 @@
+#TODO: Load models
+    
+
+#TODO: Load video
+
+
+#TODO: Read frames
+
+
+#TODO: Detect vehicles
+
+
+#TODO: Track vehicles
+
+
+#TODO: Detect license plates
+
+
+#TODO: Assign license plate to car
+
+
+#TODO: Crop license plate
+
+
+#TODO: Process license plate
+
+
+#TODO: Read license plate
+
+
+#TODO: Write results
