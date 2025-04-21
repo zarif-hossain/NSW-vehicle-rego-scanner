3abb6505707ddf6a177047de44247b28b47d6cdc
Zarif <zarif.hossain1909@gmail.com>
2025-04-21 20:30:59 +1000

    added more info into README & started implementing plate_recognition code

--- plate_recognition/main.py
+++ plate_recognition/main.py
@@ -1,34 +1,41 @@
 from ultralytics import YOLO
-
+import cv2
 
 #TODO: Load models
-    
+yolo = YOLO('yolov8n.pt')  # Load a model
+license_plate_detector = YOLO('license_plate_detector.pt')  # Load a model
+
 
 #TODO: Load video
-
+capture = cv2.VideoCapture('video.mp4')  # Load a video file
 
 #TODO: Read frames
+ret = True
+while ret:
+    ret, frame = capture.read()  # Read a frame from the video
+    if not ret:
+        break
 
 
-#TODO: Detect vehicles
+    #TODO: Detect vehicles
 
 
-#TODO: Track vehicles
+    #TODO: Track vehicles
 
 
-#TODO: Detect license plates
+    #TODO: Detect license plates
 
 
-#TODO: Assign license plate to car
+    #TODO: Assign license plate to car
 
 
-#TODO: Crop license plate
+    #TODO: Crop license plate
 
 
-#TODO: Process license plate
+    #TODO: Process license plate
 
 
-#TODO: Read license plate
+    #TODO: Read license plate
 
 
-#TODO: Write results
+    #TODO: Write results
